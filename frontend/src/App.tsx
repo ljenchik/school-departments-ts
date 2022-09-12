cefa5023fdd6f7af986a315a45294c434a6fcadf
ljenchik <84686704+ljenchik@users.noreply.github.com>
2022-09-12 14:01:56 +0100

    Added home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './css/App.css';
+import { HomePage } from './components/homePage';
 
-function App() {
+const Paths = () => {
   return (
-    <div className="App">
-      Hello world!
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+    </Routes>
   );
-}
+};
+
+
+const App = () => {
+  return (
+    <Router>
+      <main>
+        <Paths />
+      </main>
+    </Router>
+  );
+};
 
 export default App;
+
+
+
+
+
+
+
+
+
