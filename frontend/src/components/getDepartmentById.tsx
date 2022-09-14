9621582a32df5b3b450783b0ab751b4c7e930a8b
ljenchik <84686704+ljenchik@users.noreply.github.com>
2022-09-14 14:39:08 +0100

    Added delete department in frontend

--- frontend/src/components/getDepartmentById.tsx
+++ frontend/src/components/getDepartmentById.tsx
@@ -1,18 +1,23 @@
 import { useParams, useNavigate } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import {
-  getDepartmentById
+  getDepartmentById,
+  deleteDepartmentById
 } from "../apiClient";
 import Container from "react-bootstrap/esm/Container";
 import "../css/getDepartmentById.css";
+import { Button } from "react-bootstrap";
 
 export const GetDepartmentById = () => {
   const params = useParams();
-  const id: string | undefined = params.id;
+  const department_id: string | undefined = params.id;
   const [department, setDepartment] = useState<any>({});
+  const [error, setError] = useState("");
   
+  const navigate = useNavigate();
+
   useEffect(() => {
-    getDepartmentById(Number(id)).then((response) =>
+    getDepartmentById(Number(department_id)).then((response) =>
       setDepartment(response[0])
     );
   }, []);
@@ -25,7 +30,23 @@ export const GetDepartmentById = () => {
     department.updated_at = department.updated_at.split('T')[0];
   }
 
+  const deleteDepartment = () => {
+    deleteDepartmentById(Number(department_id))
+      .then((response) => {
+          if (response.success === true) {
+              navigate("/department");
+          }
+          else {
+            setError("You can't delete department with employees");
+          }
+        }
+      )};
 
+  const updateDepartment = () => {
+    getDepartmentById(Number(department_id)).then((response) =>
+      navigate(`/department/${department_id}/update`)
+    );
+  };
     return (
       <div>
         <Container>
@@ -37,6 +58,20 @@ export const GetDepartmentById = () => {
           Created on {department.created_at}
           <br/>
           Updated on {department.updated_at}
+
+          <div className="d-flex flex-row">
+            <div>
+              <Button className="btn btn-success my-2" onClick={updateDepartment}>
+                Edit
+              </Button>
+            </div>
+            <div>
+              <Button className="mx-2 my-2" onClick={deleteDepartment}>
+                Delete
+              </Button>
+            </div>
+
+        </div>
         </Container>
       </div>
     );
