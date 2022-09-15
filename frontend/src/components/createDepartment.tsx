8e3199bdbcfa496415966dcc1d6493973373571b
ljenchik <84686704+ljenchik@users.noreply.github.com>
2022-09-15 12:39:27 +0100

    Added department name and image url validation

--- frontend/src/components/createDepartment.tsx
+++ frontend/src/components/createDepartment.tsx
@@ -1,40 +1,66 @@
 import React, { useState } from "react";
 import { createDepartment } from "../apiClient";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/esm/Container";
 import { CreateDepartmentForm } from "../requestModels/departmentModels";
 import { useNavigate } from "react-router-dom";
-import "../css/createDepartment.css"
+import "../css/createDepartment.css";
 
+var validateImageUrl = new RegExp('([a-z\-_0-9\/\:\.]*\.(jpg|jpeg|png|gif))');
 
 export const CreateDepartment = () => {
   const [department, setDepartment] = useState<CreateDepartmentForm>({
     department_name: "",
-    image: ""
+    image: "",
   });
+  const [error, setError] = useState("");
   const navigate = useNavigate();
-  const handleChangeDepartmentName = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setDepartment({... department, department_name :  event.target.value});
+  const handleChangeDepartmentName = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setDepartment({ ...department, department_name: event.target.value });
+    setError("");
   };
 
-  const handleChangeDepartmentImage = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setDepartment({... department, image:  event.target.value});
+  const handleChangeDepartmentImage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setDepartment({ ...department, image: event.target.value });
+    setError("");
   };
 
   const submit = () => {
     const request: CreateDepartmentForm = {
-        department_name: "",
-        image: ""
+      department_name: "",
+      image: "",
     };
-      request.department_name = department.department_name;
-      request.image = department.image;
-      createDepartment(request).then((response) => {
-        navigate(`/`);
-      })
+
+    request.department_name = department.department_name;
+
+    if (!request.department_name) {
+      setError("Enter department name");
+    } else {
+      if (validateImageUrl.test(department.image) && department.image) {
+        request.image = department.image;
+        createDepartment(request).then((response) => {
+          navigate(`/`);
+        }
+      )}
+      else if (!department.image) {
+        request.image =
+        "https://seekvectorlogo.net/wp-content/uploads/2019/03/department-for-education-vector-logo.png";
+        createDepartment(request).then((response) => {
+          navigate(`/`);
+        }
+      )}
+      else if (validateImageUrl.test(department.image) === false) {
+        setError("Enter a valid image url");
+      }
+    }
   };
 
-  const handleKeyPress = (event: { keyCode: number; }) => {
+  const handleKeyPress = (event: { keyCode: number }) => {
     if (event.keyCode === 13) {
       submit();
     }
@@ -68,7 +94,7 @@ export const CreateDepartment = () => {
               ></input>
             </label>
           </div>
-
+          <div className="d-flex">
             <Button
               className="btn btn-success my-3"
               onKeyDown={handleKeyPress}
@@ -76,7 +102,15 @@ export const CreateDepartment = () => {
             >
               Submit
             </Button>
-            
+
+            {error !== "" ? (
+              <p className="error-message" style={{ color: "red" }}>
+                {error}
+              </p>
+            ) : (
+              ""
+            )}
+          </div>
         </fieldset>
         <br />
         <div>
