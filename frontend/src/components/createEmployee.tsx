d19dd8e758f7e2bafb74de1673aa79375299c8fa
Olena Pelagenko <84686704+ljenchik@users.noreply.github.com>
2023-01-16 22:17:35 +0000

    Updated create employee page

--- frontend/src/components/createEmployee.tsx
+++ frontend/src/components/createEmployee.tsx
@@ -38,7 +38,7 @@ export const CreateEmployee = () => {
 
   useEffect(() => {
     getDepartmentById(Number(department_id)).then((response) => {
-      setDepartmentName(response[0].department_name);
+      setDepartmentName(response.department.department_name);
     });
   }, []);
 
@@ -49,7 +49,7 @@ export const CreateEmployee = () => {
     setEmployee({ ...employee });
   };
 
-  const handleChangeEmployeeGender = (event: { target: { value: string; }; }) => {
+  const handleChangeEmployeeGender = (event: { target: { value: string } }) => {
     employee.gender = event.target.value;
     setEmployee({ ...employee });
   };
@@ -170,136 +170,138 @@ export const CreateEmployee = () => {
     });
   };
 
-  return (
-    <Container>
-      <h3 className="title">Add employee to {departmentName}</h3>
-      <fieldset onKeyDown={handleKeyPress}>
-        <div>
-          <label>Name</label>
-          <br />
-          <input
-            className="input-large-large search-query my-2 mb-3"
-            type="text"
-            style={{ width: "75%" }}
-            placeholder="Enter name"
-            onChange={(event) => handleChangeEmployeeName(event)}
-            value={employee.name}
-          ></input>
-        </div>
+  if (!departmentName) {
+    return <div>Loading ...</div>;
+  } else {
+    return (
+      <Container className="create-employee-container">
+        <h2 className="title">Add employee to {departmentName}</h2>
+        <fieldset onKeyDown={handleKeyPress}>
+          <div className="create-employee-input">
+            <label>Name</label>
+            <input
+              className="input-large-large search-query my-2 mb-3"
+              type="text"
+              style={{ width: "55%" }}
+              placeholder="Enter name"
+              onChange={(event) => handleChangeEmployeeName(event)}
+              value={employee.name}
+            ></input>
+          </div>
 
-        <div>
-          <label>Gender</label>
-          <br />
-          <select
-            className="input-large-large search-query my-2 mb-3"
-            style={{ width: "75%", height: "30px" }}
-            onChange={handleChangeEmployeeGender}
-            value={employee.gender}
-          >
-            {" "}
-            <option>Choose gender</option>
-            <option>Female</option>
-            <option>Male</option>
-          </select>
-        </div>
+          <div className="create-employee-input">
+            <label>Gender</label>
+            <select
+              className="input-large-large search-query my-2 mb-3"
+              style={{ width: "55%", height: "30px" }}
+              onChange={handleChangeEmployeeGender}
+              value={employee.gender}
+            >
+              {" "}
+              <option>Choose gender</option>
+              <option>Female</option>
+              <option>Male</option>
+            </select>
+          </div>
 
-        <div>
-          <label>Role</label> <br />
-          <input
-            className="input-large-large search-query my-2 mb-3"
-            type="text"
-            style={{ width: "75%" }}
-            placeholder="Enter role"
-            onChange={(event) => handleChangeEmployeeRole(event)}
-            value={employee.role}
-          ></input>
-        </div>
+          <div className="create-employee-input">
+            <label>Role</label>
+            <input
+              className="input-large-large search-query my-2 mb-3"
+              type="text"
+              style={{ width: "55%" }}
+              placeholder="Enter role"
+              onChange={(event) => handleChangeEmployeeRole(event)}
+              value={employee.role}
+            ></input>
+          </div>
 
-        <div>
-          <label>Date of birth</label> <br />
-          <input
-            className="input-large-large search-query my-2 mb-3"
-            type="date"
-            style={{ width: "75%" }}
-            onChange={(event) => handleChangeEmployeeDob(event)}
-            value={employee.dob}
-          ></input>
-        </div>
+          <div className="create-employee-input">
+            <label>Date of birth</label>
+            <input
+              className="input-large-large search-query my-2 mb-3"
+              type="date"
+              style={{ width: "55%" }}
+              onChange={(event) => handleChangeEmployeeDob(event)}
+              value={employee.dob}
+            ></input>
+          </div>
 
-        <div>
-          <label>Address</label> <br />
-          <input
-            className="input-large-large search-query my-2 mb-3"
-            type="address"
-            style={{ width: "75%" }}
-            placeholder="Enter address"
-            onChange={(event) => handleChangeEmployeeAddress(event)}
-            value={employee.address}
-          ></input>
-        </div>
+          <div className="create-employee-input">
+            <label>Address</label>
+            <input
+              className="input-large-large search-query my-2 mb-3"
+              type="address"
+              style={{ width: "55%" }}
+              placeholder="Enter address"
+              onChange={(event) => handleChangeEmployeeAddress(event)}
+              value={employee.address}
+            ></input>
+          </div>
 
-        <div>
-          <label>Phone</label> <br />
-          <input
-            className="input-large-large search-query my-2 mb-3"
-            type="tel"
-            style={{ width: "75%" }}
-            placeholder="+44 xxxx xxxxxx"
-            onChange={(event) => handleChangeEmployeePhone(event)}
-            value={employee.phone}
-          ></input>
-        </div>
+          <div className="create-employee-input">
+            <label>Phone</label>
+            <input
+              className="input-large-large search-query my-2 mb-3"
+              type="tel"
+              style={{ width: "55%" }}
+              placeholder="+44 xxxx xxxxxx"
+              onChange={(event) => handleChangeEmployeePhone(event)}
+              value={employee.phone}
+            ></input>
+          </div>
 
-        <div>
-          <label>Email</label> <br />
-          <input
-            className="input-large-large search-query my-2 mb-3"
-            type="email"
-            style={{ width: "75%" }}
-            placeholder="Enter email address"
-            onChange={(event) => handleChangeEmployeeEmail(event)}
-            value={employee.email}
-          ></input>
-        </div>
+          <div className="create-employee-input">
+            <label>Email</label>
+            <input
+              className="input-large-large search-query my-2 mb-3"
+              type="email"
+              style={{ width: "55%" }}
+              placeholder="Enter email address"
+              onChange={(event) => handleChangeEmployeeEmail(event)}
+              value={employee.email}
+            ></input>
+          </div>
 
-        <div>
-          <label>Start date</label> <br />
-          <input
-            className="input-large-large search-query my-2 mb-3"
-            type="date"
-            style={{ width: "75%" }}
-            onChange={(event) => handleChangeEmployeeStartDate(event)}
-            value={employee.start_date}
-          ></input>
-        </div>
+          <div className="create-employee-input">
+            <label>Start date</label>
+            <input
+              className="input-large-large search-query my-2 mb-3"
+              type="date"
+              style={{ width: "55%" }}
+              onChange={(event) => handleChangeEmployeeStartDate(event)}
+              value={employee.start_date}
+            ></input>
+          </div>
 
-        <div>
-          <label>Salary £</label> <br />
-          <input
-            className="input-large-large search-query my-2 mb-3"
-            type="number"
-            style={{ width: "75%" }}
-            onChange={(event) => handleChangeEmployeeSalary(event)}
-            value={employee.salary}
-          ></input>
-        </div>
+          <div className="create-employee-input">
+            <label>Salary £</label>
+            <input
+              className="input-large-large search-query my-2 mb-3"
+              type="number"
+              style={{ width: "55%" }}
+              onChange={(event) => handleChangeEmployeeSalary(event)}
+              value={employee.salary}
+            ></input>
+          </div>
 
-        <div>
-          <label>Photo (optional)</label> <br />
-          <input
-            className="input-large-large search-query my-2 mb-3"
-            type="url"
-            placeholder="https://example.com"
-            pattern="https://.*"
-            style={{ width: "75%" }}
-            onChange={(event) => handleChangeEmployeePhoto(event)}
-            value={employee.photo}
-          ></input>
-        </div>
+          <div className="create-employee-input">
+            <label>Photo (optional)</label>
+            <input
+              className="input-large-large search-query my-2 mb-3"
+              type="url"
+              placeholder="https://example.com"
+              pattern="https://.*"
+              style={{ width: "55%" }}
+              onChange={(event) => handleChangeEmployeePhoto(event)}
+              value={employee.photo}
+            ></input>
+          </div>
+        </fieldset>
 
-        <div className="d-flex flex-row">
+        <div className="d-flex flex-row buttons">
           <Button
-            className="btn btn-success my-4"
+            className="btn btn-success my-4 mx-2"
             disabled={isDisabled}
             onClick={submit}
           >
@@ -308,30 +310,31 @@ export const CreateEmployee = () => {
           <Button className="mx-2 my-4" onClick={reset}>
             Reset
           </Button>
+
           <p className="error">
             {error !== "" ? <p style={{ color: "red" }}>{error}</p> : ""}
           </p>
         </div>
-      </fieldset>
 
-      <div>
-        <Link to={`/department/${department_id}`} className="link">
-          {" "}
-          View all employees of {departmentName}
-        </Link>
-      </div>
-      <div>
-        <Link to="/department" className="link">
-          {" "}
-          View all departments{" "}
-        </Link>
-      </div>
-      <div>
-        <Link to="/employee" className="link">
-          {" "}
-          View all employees{" "}
-        </Link>
-      </div>
-    </Container>
-  );
+        <div className="links">
+          <div>
+            <Link to={`/department/${department_id}`} className="link">
+              Employees of {departmentName}
+            </Link>
+          </div>
+          <div>
+            <Link to="/employee" className="link">
+              Employees
+            </Link>
+          </div>
+          <div>
+            <Link to="/" className="link">
+              Departments
+            </Link>
+          </div>
+          
+        </div>
+      </Container>
+    );
+  }
 };
