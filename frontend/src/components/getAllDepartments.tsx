6b16bbd4761cd8f4f3aa34adeb11a038a5c8f28f
Olena Pelagenko <84686704+ljenchik@users.noreply.github.com>
2023-01-16 15:27:45 +0000

    Changed layout of the home page, used flexbox, added shadow-box to highlight cards

--- frontend/src/components/getAllDepartments.tsx
+++ frontend/src/components/getAllDepartments.tsx
@@ -12,7 +12,6 @@ import { Link } from "react-router-dom";
 import "../css/getAllDepartments.css";
 import { Container } from "react-bootstrap";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import Carousel from "better-react-carousel";
 
 export const GetAllDepartments = () => {
   const [departments, setDepartments] = useState<any>([]);
@@ -24,63 +23,49 @@ export const GetAllDepartments = () => {
   }, []);
 
   return (
-    <Container >
+    <Container>
       <h2 className="title">Departments</h2>
-      <br />
-      <Carousel cols={4} rows={1} gap={20} loop style={{ margin: "0px" }}>
-        {departments.map(
-          (department: {
-            id: any;
-            image: string | undefined;
-            department_name: any;
-          }) => (
-            <Carousel.Item>
-              <Link to={`/department/${department.id}`} className="link">
-                <div className="link-dep-name">{department.department_name}</div>
-                <img src={department.image} className="link-dep-logo"/>
-              </Link>
-            </Carousel.Item>
-          )
-        )}
 
-        <Carousel.Item>
-          <Link to={`/department/create`} className="link">
-            <div className="link-dep-name">Add new department</div>
-            <img
-              src="https://www.villageofallouezwi.gov/wp-content/uploads/2013/04/departments.jpg"
-              className="link-dep-logo"
-            />
-          </Link>
-        </Carousel.Item>
-      </Carousel>
+      <div className="dep-container">
+            {departments.map(
+              (department: {
+                id: any;
+                image: string | undefined;
+                department_name: any;
+              }) => (
 
-      <br />
-      {departments.map(
-        (department: {
-          id: Key | null | undefined;
-          department_name:
-            | string
-            | number
-            | boolean
-            | ReactElement<any, string | JSXElementConstructor<any>>
-            | ReactFragment
-            | ReactPortal
-            | null
-            | undefined;
-        }) => (
-          <div key={department.id}>
-            <Link to={`/department/${department.id}`} className="link">
-              {" "}
-              {department.department_name}{" "}
-            </Link>
-          </div>
-        )
-      )}
-      <br />
-      <Link to="/department/create" className="add-link">
-        {" "}
-        Add a new department{" "}
-      </Link>
+                  <div className="card">
+                    <div className="card-body">
+                      <Link
+                        to={`/department/${department.id}`}
+                        className="link"
+                      >
+                        <h4 className="card-title">
+                          <div className="link-dep-name">
+                            {department.department_name}
+                          </div>
+                        </h4>
+                        <img src={department.image} className="link-dep-logo" />
+                      </Link>
+                    </div>
+                  </div>
+              )
+            )}
+
+              <div className="card">
+                <div className="card-body">
+                  <Link to={`/department/create`} className="link">
+                    <h4 className="card-title">
+                      <div className="link-dep-name">Add new department</div>
+                    </h4>
+                    <img
+                      src="https://www.villageofallouezwi.gov/wp-content/uploads/2013/04/departments.jpg"
+                      className="link-dep-logo"
+                    />
+                  </Link>
+                </div>
+              </div>
+            </div>
     </Container>
   );
 };
